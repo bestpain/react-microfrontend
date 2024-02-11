04ff26f461b374aae4aac6a8f93789cf789f29b8
bestpain <nitishranjan67@gmail.com>
2024-02-11 23:54:49 +0530

    added container & products module fedration

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,10 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 module.exports = {
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            name:'container',
+            remotes:{
+                productsApp:'product@http://localhost:8081/remoteEntry.js'
+            }
         })
     ],
     devServer: {
--- products/src/index.js
+++ products/src/index.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName();
     products += `<div>${name}</div>`
 }
+console.log("products file")
 
 // let rootElement =  document.getElementById('products')
 // rootElement.innerHTML = products
